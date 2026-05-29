257d9b68b0075860feb2def0938586ff2d48f286
Kotela <katherinamartsynkevich@gmail.com>
2026-05-29 22:57:18 +0200

    Change functions habit_list, view_habit, edit_habit, create_habit, delete_habit to class-based views. Add HTML habit_confirm_delete.

--- habits/urls.py
+++ habits/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from habits import views
 
 urlpatterns = [
-    path('', views.habit_list, name='habit_list'),
-    path('habits/<int:habit_id>/', views.view_habit, name='view_habit'),
-    path('habits/complete/<int:habit_id>/', views.complete_habit, name='complete_habit'),
-    path('habits/add/', views.add_habit, name='add_habit'),
-    path('habits/delete/<int:habit_id>/', views.delete_habit, name='delete_habit'),
-    path('habits/edit/<int:habit_id>/', views.edit_habit, name='edit_habit')
+    path('', views.ListHabits.as_view(), name='habit_list'),
+    path('habits/<int:habit_id>/', views.DetailHabit.as_view(), name='view_habit'),
+    path('habits/add/', views.CreateHabit.as_view(), name='add_habit'),
+    path('habits/delete/<int:habit_id>/', views.DeleteHabit.as_view(), name='delete_habit'),
+    path('habits/edit/<int:habit_id>/', views.UpdateHabit.as_view(), name='edit_habit'),
+    path('habits/complete/<int:habit_id>/', views.complete_habit,
+         name='complete_habit')
 ]
